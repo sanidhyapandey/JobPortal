dbb5ebfb31843569820ef101d1ad970189c82f0f
Sanidhya Pandey <sanidhya.pandey@tadigital.com>
2020-12-21 20:58:04 +0530

    search functionality done

--- app/main.js
+++ app/main.js
@@ -2,54 +2,31 @@ var xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'data.json', true);
 xhr.responseType = 'text';
+var myData;
 
 xhr.onload = function () {
     if (xhr.status === 200) {
-        var myData = JSON.parse(xhr.responseText);
+        myData = JSON.parse(xhr.responseText);
         console.log(myData);
-        // document.getElementById('message').innerHTML = myString;
-        //ul = document.getElementById("users-list");
-
-        // let render_lists = function(lists){
-        //   let li = "";
-        //   for(index in lists){
-        //     li += "<li>" + lists[index] + "</li>";
-        //   }
-        //   //ul.innerHTML = li;
-        // }
-
-        //render_lists(myData.jobs);
-        function savefn() {
-        var myString = "";
-        var input = document.getElementById('search');
-        var keyword = input.value.toLowerCase();
-        console.log(keyword);
-
-        for (i=0; i<myData.jobs.length; i++) {
-            if(keyword === myData.jobs.profile[i])
-            {
-                myString = myData.jobs[i];
-            }
-            document.getElementById('message').innerHTML = myString;
-        }
-
-        let filterUsers = function (event) {
-            
-            filtered_users = myData.jobs.filter(function (user) {
-                console.log(myData.jobs);
-                //  user = user.toLowerCase();
-                // return user.indexOf(keyword) > -1; 
-            });
-
-            //render_lists(filtered_users);
-        }
-
-        input.addEventListener('keyup', filterUsers);
     }
+}
+function saveInput() {
+    var myString = "";
+    var input = document.getElementById('search');
+    console.log(input.value);
+    var keyword = input.value.toLowerCase();
+
+
+    for (i = 0; i < myData.jobs.length; i++) {
+        var data = JSON.stringify(myData.jobs[i]).toLowerCase();
+        if (data.search(keyword) != -1) {
+            console.log(myData.jobs[i]);
+            myString += JSON.stringify(myData.jobs[i]);
+        }
+        document.getElementById('message').innerHTML = myString;
     }
+
 }
 
 
 xhr.send();
-// var mydata = JSON.parse(jobs);
-// alert(mydata[0].profile);
