3bdc03965c67465951167bcdf875fea8d335c11f
Sanidhya Pandey <sanidhya.pandey@tadigital.com>
2021-01-02 19:41:00 +0530

    Updated Code

--- app/main.js
+++ app/main.js
@@ -8,43 +8,10 @@ async function gettingData() {
 
 gettingData();
 
-// var xhr = new XMLHttpRequest();
-
-// xhr.open('GET', 'data.json');
-// xhr.responseType = 'text';
-// var myData;
-
-// // Load JSON Data 
-// xhr.onload = function () {
-//     if (xhr.status === 200) {
-//         myData = JSON.parse(xhr.responseText);
-//         console.log(myData);
-//     }
-// }
-
 var searchInput = document.getElementById('search');   // Get Search Input Value
 console.log(searchInput.value);
 
-// searchInput.addEventListener('input', updateValue);
 
-// function updateValue(e) {
-//     document.getElementById('holder1').innerHTML = "";
-//     if (e.target.value.length > 2) {
-//         for (i = 0; i < myData.jobs.length; i++) {
-//             var data = JSON.stringify(myData.jobs[i]).toLowerCase();
-//             console.log(e.target.value);
-//             if (data.search(e.target.value) != -1) {
-//                 console.log(myData.jobs[i]);
-//                 var elem = document.createElement('div');
-//                 elem.setAttribute("id", "job");
-
-//                 elem.innerHTML = '<h2 id="profile">' + "Profile : " + myData.jobs[i].profile + '</h2>' + '<h3 id="company">' + "Company : " + myData.jobs[i].company + '</h3>' + '<h3 id="salary">' + "Salary : " + myData.jobs[i].salary + '</h3>' + '<h3 id="location">' + "Location : " + myData.jobs[i].location + '</h3>' + ' <a id="apply" href="./job_description.html">Apply Now</a>';
-
-//                 document.getElementById('holder1').appendChild(elem);
-//             }
-//         }
-//     }
-// }
 
 console.log(myData);
 
